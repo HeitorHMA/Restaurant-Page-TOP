b81131555d5f2cd08439a9bcbec62c33f65c6519
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-02-20 07:30:50 -0300

    Delete index.html skeleton and create elements with javascript

--- src/homePage.js
+++ src/homePage.js
@@ -2,10 +2,19 @@ function createHomePage(){
 
     const content = document.querySelector("#content");
     const pageContent = document.createElement ("div");
+    content.appendChild(pageContent);
 
     const image = document.createElement("img");
     image.src ='/imgs/restaurant-page.jpg';
     image.height = "400";
-    pageContent.appendChild("image")
+    pageContent.appendChild(image);
+
+    const title = document.createElement("h1");
+    title.textContent ="Welcome to our restaurant";
+    pageContent.appendChild(title);
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "the best food in Paris";
+    pageContent.appendChild(paragraph);
 }
 export default createHomePage;
