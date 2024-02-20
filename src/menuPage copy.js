b6e63756cb2ae3c9da8532bbeb0c2c9eeeb28800
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-02-20 07:47:38 -0300

    Create Tab buttons function

---
+++ src/menuPage copy.js
@@ -0,0 +1,7 @@
+function tabButtons(){
+
+    const homeButton = document.querySelector("#homeButton")
+    const supportButton = document.querySelector("#supportButton")
+    const menuButton = document.querySelector("#menuButton")
+}
+export default createMenuPage;
